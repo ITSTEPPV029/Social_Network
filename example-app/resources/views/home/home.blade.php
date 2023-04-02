616a4d4ea842983b693c7fd25d63c82615ab2197
vitalikrez <vitalik.rez@gmail.com>
2023-04-02 22:23:45 +0300

    First test commit

--- example-app/resources/views/home/home.blade.php
+++ example-app/resources/views/home/home.blade.php
@@ -3,13 +3,26 @@
 @section('content')
 
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
-      <h1 class="display-4 fw-normal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Карикатурный заголовок</font></font></h1>
-      <p class="lead fw-normal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">И еще более остроумный подзаголовок в придачу. </font><font style="vertical-align: inherit;">Ускорьте свои маркетинговые усилия с помощью этого примера, основанного на маркетинговых страницах Apple.</font></font></p>
-      <a class="btn btn-outline-secondary" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Вскоре</font></font></a>
-    </div>
-    <div class="product-device shadow-sm d-none d-md-block"></div>
-    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <h1 class="display-4 fw-normal">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Привіт!!!</font>
+      </font>
+    </h1>
+    <p class="lead fw-normal">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Це головна сторінка<br></font>
+        <font style="vertical-align: inherit;">Тут буде відображена основна інфомомація</font>
+      </font>
+    </p>
+    <a class="btn btn-outline-secondary" href="#">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Кнопка</font>
+      </font>
+    </a>
   </div>
- 
+  <div class="product-device shadow-sm d-none d-md-block"></div>
+  <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
+
 @endsection
