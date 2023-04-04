772f54d57e94fd4a933590229f6c191a139ff7b2
Domitory123 <ddmitromaas@gmail.com>
2023-04-04 17:36:27 +0300

    тест3

--- example-app/app/Http/Controllers/UserController.php
+++ example-app/app/Http/Controllers/UserController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
 use Illuminate\Support\Facades\Auth;
+use vendor\Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
-    //test1
+
    /**
      * запит на дружбу
      *
      * @param  \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
-    //test1
+
     public function friendRequest(User $user)
     {
         $User = \App\Models\User::find(Auth::user()->id);
@@ -35,5 +35,32 @@ class UserController extends Controller
         $User->friendsRequest()->where('id',$user->id)->first()->pivot->update(['friend_request'=>true]);
         return view('home/home');
     }
+
+  /**
+     * добавлення аватарки
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function addAvatar(Request $request)
+    {   
+          
+          $fileName= $request->file('avatar');
+          $user =  new User();
+          $filePath = $user->getAvatarPath(Auth::user()->id);
+         // Image::make($fileName)->resize(300, 200)->save(public_path($filePath.$fileName));
+          
+          $photoMainName = $request->file('avatar')->store('uploads','public');
+
+      // $merch->name_main_photo = $photoMainName;
+
+       // $request
+        //$User = \App\Models\User::find(Auth::user()->id);
+       // $User->friendsRequest()->where('id',$user->id)->first()->pivot->update(['friend_request'=>true]);
+       // return view('home/home');
+    }
+
+
+
 
 }
