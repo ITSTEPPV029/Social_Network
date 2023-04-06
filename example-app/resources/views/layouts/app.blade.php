fb4e9515cb1821babd329fbf4814daeea94f8d87
Domytori <dmitromaas2@gmail.com>
2023-04-06 22:11:10 +0300

    add

--- example-app/resources/views/layouts/app.blade.php
+++ example-app/resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">  
         <title>Test name</title> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+          <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">           
     </head>
     <body >
 
