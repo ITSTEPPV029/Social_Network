b0ea9c67af73060a95439e10937715664bad875e
Andrii <andrmel9245@gmail.com>
2023-04-26 23:42:27 +0300

    firstHeader (not real logo)

--- example-app/resources/views/inc/header.blade.php
+++ example-app/resources/views/inc/header.blade.php
@@ -3,30 +3,30 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Начальная загрузка"><use xlink:href="#bootstrap"></use></svg>
+          <img class="bi me-2" alt="Начальная загрузка" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
         </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="\" class="nav-link px-2 text-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Дом</font></font></a></li>
-          <li><a href="{{ route('login.getSigin') }}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Функции</font></font></a></li>
+        
+        <ul class="nav">
+          <li><a href="\" class="nav-link text-secondary">Головна</a></li>
+          <li><a href="{{ route('login.getSigin') }}" class="nav-link">Новини</a></li>
           <li></li>
-          <li><a href="{{ route('allUser.allUser') }}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">всі користувачі</font></font></a></li>
+          <li><a href="{{ route('allUser.allUser') }}" class="nav-link">всі користувачі</a></li>
          
         </ul>
 
-        <form  action="{{route('search.searchUser')}}"  method="get" enctype="multipart/form-data" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input name="search" type="search" class="form-control form-control-dark text-bg-dark" placeholder="Поиск..." aria-label="Поиск">
-          <input class="btn btn-outline-light me-2" class="inputSubmit" type="submit"  value="пошук"></input>
+        <form  action="{{route('search.searchUser')}}"  method="get" enctype="multipart/form-data" role="search">
+          <input name="search" type="search" class="form-control-dark text-bg-dark" placeholder="Поиск..." aria-label="Поиск">
+          <input class="btn btn-outline-light" class="inputSubmit" type="submit" value="пошук"/>
         </form>
 
         @if(Auth::check())
-         <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">мій профіль</font></font></a>
+         <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link px-2 text-white">мій профіль</a>
          <b class="nav-link px-2 text-white" style="vertical-align: inherit;" > вітаємо  {{ Auth::user()->last_name }}!</b>
-          <button type="button" class="btn btn-warning"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><a href="{{ route('logout.getSigout') }}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Вийти</font></font></a></font></font></button>
+          <button type="button" class="btn btn-warning"><a href="{{ route('logout.getSigout') }}" class="nav-link px-2 text-white">Вийти</a></button>
           @else
           <div class="text-end"> 
-            <button type="button" class="btn btn-outline-light me-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><a href="{{ route('registration.getSigUp') }}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Реєстрація</font></font></a></font></font></button>
-            <button type="button" class="btn btn-warning"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><a href="{{ route('login.getSigin') }}" class="nav-link px-2 text-white"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Увійти</font></font></a></font></font></button>
+            <button type="button" class="btn btn-outline-light me-2"><a href="{{ route('registration.getSigUp') }}" class="nav-link px-2 text-white">Реєстрація</a></button>
+            <button type="button" class="btn btn-warning"><a href="{{ route('login.getSigin') }}" class="nav-link px-2 text-white">Увійти</a></button>
           </div>
         @endif
 
