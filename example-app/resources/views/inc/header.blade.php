2d33b909a77c589bc0bab4b875de12df390b35a9
Andrii <andrmel9245@gmail.com>
2023-04-30 20:38:53 +0300

    header0.2

--- example-app/resources/views/inc/header.blade.php
+++ example-app/resources/views/inc/header.blade.php
@@ -2,17 +2,18 @@
 <header class="p-3 text-bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <!--Лого-->
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
           <img class="bi me-2" alt="Начальная загрузка" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
         </a>
         
-        <ul class="nav">
+        <!-- <ul class="nav">
           <li><a href="\" class="nav-link text-secondary">Головна</a></li>
           <li><a href="{{ route('login.getSigin') }}" class="nav-link">Новини</a></li>
           <li></li>
           <li><a href="{{ route('allUser.allUser') }}" class="nav-link">всі користувачі</a></li>
          
-        </ul>
+        </ul> -->
 
         <form  action="{{route('search.searchUser')}}"  method="get" enctype="multipart/form-data" role="search">
           <input name="search" type="search" class="form-control-dark text-bg-dark" placeholder="Поиск..." aria-label="Поиск">
@@ -20,7 +21,8 @@
         </form>
 
         @if(Auth::check())
-         <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link px-2 text-white">мій профіль</a>
+        <a class="nav-link" href="#"><img src="{{'notification.png'}}"/></a>
+         <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link text-white">мій профіль</a>
          <b class="nav-link px-2 text-white" style="vertical-align: inherit;" > вітаємо  {{ Auth::user()->last_name }}!</b>
           <button type="button" class="btn btn-warning"><a href="{{ route('logout.getSigout') }}" class="nav-link px-2 text-white">Вийти</a></button>
           @else
