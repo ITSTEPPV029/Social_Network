e8622bfbb6f98e6b9c85bf83603a6a1f10475123
Domytori <dmitromaas2@gmail.com>
2023-05-09 08:07:07 +0300

    map js

--- example-app/resources/js/components/MyMap.vue
+++ example-app/resources/js/components/MyMap.vue
@@ -1,14 +1,34 @@
 <template>
+    <div class="myMap" id="map"></div>
+  </template>
+  
+  <script>
+  import L from 'leaflet';
+  
+  export default {
+    mounted() {
+      // Створення карти
+      this.map = L.map('map').setView([51.505, -0.09], 13);
+  
+      // Додавання плитки карти
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+      }).addTo(this.map);
+  
+      //Додавання маркера на карту
+      L.marker([51.5, -0.09]).addTo(this.map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+  };
+  </script>
+  
+  <style>
+  .myMap {
     
-    <h1>123</h1>
+  width: 40%;
+  height: 400px;
 
-
-</template>
-
-<script>
-
-export default {
-
-
-}
-</script>
+  }
+  </style>
--- example-app/resources/views/layouts/app.blade.php
+++ example-app/resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
         <title>Pet Life</title> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
           <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">           
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">   
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     </head>
     <body >
 
