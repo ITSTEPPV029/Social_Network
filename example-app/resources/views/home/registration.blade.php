3f335b08ed145a7ab2aabc9b9db13b732a792ba2
vitalik.rez <vitalik.rez@gmail.com>
2023-05-10 19:04:20 +0300

    add registration page v.1

--- example-app/resources/views/home/registration.blade.php
+++ example-app/resources/views/home/registration.blade.php
@@ -2,93 +2,64 @@
 
 @section('content')
 
-<br /> <br />
-<form class="row g-3" action="{{ route('registration.postSigUp')}}" method="post" enctype="multipart/form-data">
-    {{ csrf_field() }}
-    <div class="row mb-3">
-        <label for="nick_name" class="col-md-4 col-form-label text-md-end">{{ __('Нікнейм') }}</label>
-        <div class="col-md-6">
-            <input id="nick_name" type="text" class="form-control @error('nick_name') is-invalid @enderror" name="nick_name" value="{{ old('nick_name') }}" required autocomplete="name" autofocus>
-            @error('nick_name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
+<div class="form-container">
+
+    <form action="{{ route('registration.postSigUp')}}" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <label>
+            <h1><b>Створити акаунт</b></h1>
+        </label>
+        <div class="social-icons">
+            <a href="https://www.facebook.com" class="facebook-icon"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Facebook_colored_svg_copy-64.png" alt="Facebook"></a>
+            <a href="https://appleid.apple.com/sign-in" class="apple-icon"><img src="https://cdn3.iconfinder.com/data/icons/picons-social/57/56-apple-64.png" alt="Apple"></a>
+            <a href="#https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fwww.google.com.ua%2F%3Fhl%3Dru&ec=GAZAmgQ&hl=ru&passive=true&ifkv=Af_xneHp-DPJvUNvMPx9nVd7LARzhxxkJVrsjkgo6uU-Edf6OOPUHOE6xqXl-eM32eKqXwd83PD4kw&flowName=GlifWebSignIn&flowEntry=ServiceLogin" class="google-icon"><img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google-64.png" alt="Google"></a>
         </div>
-    </div>
+        <label>
+            <h2><b>Або</b></h2>
+        </label>
+
+        <input class="form-control @error('first_name') is-invalid @enderror" type="text" id="first_name" name="first_name" value="{{ old('first_name') }}" placeholder="Ім’я" autofocus required>
+        @error('first_name')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+
+        <input class="form-control @error('last_name') is-invalid @enderror" id="last_name" type="text" name="last_name" value="{{ old('last_name') }}" placeholder="Прізвище" required>
+        @error('last_name')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+
+        <input class="form-control @error('email') is-invalid @enderror" id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Електронна пошта" required>
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+
+        <input class="form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Пароль" required>
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+
+        <input class="form-control @error('confirm_password') is-invalid @enderror" type="password" id="confirm_password" name="confirm_password" placeholder="Підтвердження паролю" required>
+        @error('confirm_password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+        <label class="conditions"> Під час входу чи реєстрації Ви погоджуєтеся з нашими Умови користування. </label>
+
+        <button type="submit">Зареєструватись</button>
 
 
-    <div class="row mb-3">
-        <label for="first_name" class="col-md-4 col-form-label text-md-end">{{ __('Імя') }}</label>
-        <div class="col-md-6">
-
-            <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
-            @error('first_name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <label for="last_name" class="col-md-4 col-form-label text-md-end">{{ __('Прізвище') }}</label>
-        <div class="col-md-6">
-
-            <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
-            @error('last_name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
 
 
-    <div class="row mb-3">
-        <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Пошта') }}</label>
+    </form>
+    <div>
 
-        <div class="col-md-6">
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Пароль') }}</label>
-
-        <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end">{{ __('Підтвердити пароль') }}</label>
-
-        <div class="col-md-6">
-            <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-        </div>
-    </div>
-
-    <div class="row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Реєстрація') }}
-            </button>
-        </div>
-    </div>
-</form>
-
-
-@endsection
+        @endsection
