bc3c0cff97c72c712f7eb9b492a1352acae47f37
Domytori <dmitromaas2@gmail.com>
2023-05-12 10:05:54 +0300

    add sideMenu

---
+++ example-app/resources/views/inc/sideMenu.blade.php
@@ -0,0 +1,10 @@
+@section("sideMenu")
+
+    <nav class="sideMenuContainer">
+      <ul>
+        <li><a href="{{ route('chat') }}" >Чат</a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </nav>
+    
