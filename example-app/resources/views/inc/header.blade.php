9028c088b99da58846d8cff5f106f221592434e9
Andrii <andrmel9245@gmail.com>
2023-05-16 22:31:54 +0300

    SideMenuHeader2

--- example-app/resources/views/inc/header.blade.php
+++ example-app/resources/views/inc/header.blade.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Auth;
 ?>
 @section("header")
-<header class="text-bg-dark">
+<header>
   <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+    <div>
       <!--Лого-->
-      <a href="/" class="logo d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-        <img class="bi me-2" alt="Начальная загрузка" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+      <a href="/" class="logo">
+        <img alt="Начальная загрузка" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
       </a>
 
       <!--<ul class="nav">
@@ -19,21 +19,21 @@ use Illuminate\Support\Facades\Auth;
       </ul>-->
 
       <form action="{{route('search.searchUser')}}" method="get" enctype="multipart/form-data" role="search">
-        <input name="search" type="search" class="form-control-dark text-bg-dark" placeholder="Поиск..." aria-label="Поиск">
-        <input class="btn btn-outline-light" class="inputSubmit" type="submit" value="пошук" />
+        <input name="search" type="search" placeholder="Поиск..." aria-label="Поиск">
+        <input class="inputSubmit" type="submit" value="пошук" />
       </form>
 
       <div class="auth">
         @if(Auth::check())
         <a class="nav-link" href="#"><img src="https://cdn-icons-png.flaticon.com/512/3239/3239958.png" /></a> <!--Сповіщення-->
-        <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link text-white">мій профіль</a>
-        <b class="nav-link text-white" style="vertical-align: inherit;"> {{ Auth::user()->last_name }}</b>
+        <a href="{{route('profile.show',['user' => Auth::user()])}}" class="nav-link">мій профіль</a>
+        <b class="nav-link" style="vertical-align: inherit;"> {{ Auth::user()->last_name }}</b>
         <a class="nav-link" href="#"><img src="<?=Auth::user()->avatar?>" /></a><!--Аватар-->
-        <button type="button" class="btn btn-warning"><a href="{{ route('logout.getSigout') }}" class="nav-link px-2 text-white">Вийти</a></button>
+        <button type="button"><a href="{{ route('logout.getSigout') }}" class="nav-link">Вийти</a></button>
         @else
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2"><a href="{{ route('registration.getSigUp') }}" class="nav-link px-2 text-white">Реєстрація</a></button>
-          <button type="button" class="btn btn-warning"><a href="{{ route('login.getSigin') }}" class="nav-link px-2 text-white">Увійти</a></button>
+        <div>
+          <button type="button"><a href="{{ route('registration.getSigUp') }}" class="nav-link">Реєстрація</a></button>
+          <button type="button"><a href="{{ route('login.getSigin') }}" class="nav-link">Увійти</a></button>
         </div>
         @endif
       </div>
