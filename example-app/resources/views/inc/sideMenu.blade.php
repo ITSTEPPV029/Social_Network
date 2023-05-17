f37131c164ee49ce3d20d818ff769f53183e4e73
Domytori <dmitromaas2@gmail.com>
2023-05-17 08:37:03 +0300

    add login page and route menu

--- example-app/resources/views/inc/sideMenu.blade.php
+++ example-app/resources/views/inc/sideMenu.blade.php
@@ -2,14 +2,15 @@
 
     <nav class="sideMenu-container">
       <ul>
-        <li><a href="{{ route('index') }}">Головна сторінка</a></li>
-        <li><a href="{{ route('index') }}">Моя сторінка</a></li>
-        <li><a href="{{ route('index') }}">Додати опис</a></li>
-        <li><a href="{{ route('index') }}">Друзі</a></li>
+        <li><a href="\">Головна сторінка</a></li>
+        <li><a href="{{ route('profile.show',['user' => Auth::user()]) }}">Моя сторінка</a></li>
+        <li><a href="{{ route('home.home') }}">Додати опис</a></li>
+        <li><a href="{{ route('allUser.allUser') }}">Друзі</a></li>
+        <li><a href="{{ route('message') }}" >повідомлення</a></li>
         <li><a href="{{ route('chat') }}" >Чат</a></li>
-        <li><a href="#">Збережені дописи</a></li>
-        <li><a href="#">Налаштування</a></li>
-        <li><a href="{{ route('index') }}">Вихід</a></li>
+        <li><a href="\">Збережені дописи</a></li>
+        <li><a href="\">Налаштування</a></li>
+        <li><a href="\">Вихід</a></li>
         
       </ul>
       <div></div>
