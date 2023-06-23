d7c618710f762f59877d6ba129c127d4e9e5d574
Andrii <andrmel9245@gmail.com>
2023-06-23 15:28:43 +0300

    WideHomePage1

--- example-app/resources/views/home/home.blade.php
+++ example-app/resources/views/home/home.blade.php
@@ -2,17 +2,83 @@
 
 @section('contentGuest')
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
+
+<script>
+  let hidden = true;
+
+  function changeDisplay(elem) {
+    let img = elem.getElementsByTagName('img');
+    if (hidden) {
+      document.getElementById("moreHome").style.setProperty('display', 'block');
+      if(img != null)
+        img[0].style.setProperty('transform', 'rotateZ(180deg)');
+    } else {
+      document.getElementById("moreHome").style.setProperty('display', 'none');
+      if(img != null)
+        img[0].style.setProperty('transform', 'rotateZ(-180deg)');
+    }
+    hidden = !hidden;
+  }
+</script>
+
+<div class="homeMain">
+  <div class="homeDiv">
+    <div class="logo-main">
+      <img src="/img/PetLifeLogo_Line.png" />
+    </div>
+    <div>
+      <h2>Знайди друзів для себе і свого улюбленця</h2>
+      <a href="#">Увійти</a>
+      <a href="#">Зареєструватися</a>
+    </div>
+    <div class="imageDog" style="top: 4.5rem; left: 56.5%;">
+      <img src="/img/dog.png" />
+    </div>
+  </div>
+
+  <div class="moreNext">
+    <button id="moreBtn" onclick="changeDisplay(this)">Дізнатися більше </br>
+      <img src="/img/doubleArrowDown.svg" /></button>
+  </div>
+  <!-- _______________________________________Для виділення головного екрану__________________________________________________ -->
+  <div id="moreHome" style="display: none;">
+    <div class="moreDiv">
+      <img src="/img/PetLifeLogo_Line.png" />
+      <div>
+        <h2>Діліться з своїми моментами з іншими</h2>
+        <div class="video-frame">Тут буде відео</div>
+      </div>
+      <div class="imageDog" style="top: 44.5%; left: 45%;">
+        <img src="/img/dog.png" style="height: 450px;" />
+      </div>
+    </div>
+    <div class="moreDiv">
+      <img src="/img/PetLifeLogo_Line.png" />
+      <div>
+        <div class="video-frame">Тут буде відео</div>
+        <h2>Знаходьте компанію для себе і своїх улюбленців</h2>
+      </div>
+      <div class="imageDog" style="top: 82%; left: -40%;">
+        <img src="/img/cat.png" style="height: 450px;" />
+      </div>
+    </div>
+    <p>
+      <a href="#">Зареєструватися</a>
+    </p>
+  </div>
+
+  <div>
+    <!-- <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 fw-normal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">В нас и нтерфейc в якому розбереться навіть ваший собака </font></font></h1>
       <p class="lead fw-normal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">И еще более остроумный подзаголовок в придачу. </font><font style="vertical-align: inherit;">Ускорьте свои маркетинговые усилия с помощью этого примера, основанного на маркетинговых страницах Apple.</font></font></p>
       <a class="btn btn-outline-secondary" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Вскоре</font></font></a>
-    </div>
+    </div> -->
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
   </div>
   <div class="product-device shadow-sm d-none d-md-block"></div>
   <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
 </div>
+
 
 @endsection
